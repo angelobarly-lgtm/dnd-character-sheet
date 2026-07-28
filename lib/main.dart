20e8c2cfa488a91c0097dfae55a691a533a92784
angelobarly-lgtm <angelo.barly@gmail.com>
2026-07-28 00:58:54 +0000

    Remove extra closing brace

--- lib/main.dart
+++ lib/main.dart
@@ -146,7 +146,6 @@ class _HomePageState extends State<HomePage>{
 ),
 );
   }
-}
 
 enum StatMethod{standard,pointBuy,dice,manual}
 class CreatorPage extends StatefulWidget{const CreatorPage({super.key});@override State<CreatorPage> createState()=>_CreatorPageState();}
